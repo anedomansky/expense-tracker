5745d3404ad106a2aec0cdc2559577de27b589f5
Alexander Nedomansky <anedomansky@googlemail.com>
2019-10-06 10:33:21 +0200

    Removed the frontend part.

--- frontend/src/components/category/CategoryForm.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const CategoryForm: React.FC = () => (
-    <div className="category-list__form">
-        CategoryForm works!
-    </div>
-);
-
-export default CategoryForm;
--- frontend/src/components/expense/ExpenseForm.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ExpenseForm: React.FC = () => (
-    <div className="expense-list__form">
-        ExpenseForm works!
-    </div>
-);
-
-export default ExpenseForm;
